13b3c034574b533f5abece54b572ea0bedd1c708
Marcio Maganha Silva <giovannifelipeguilherme@hotmail.com>
2021-06-13 10:50:07 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusProjeto
+{
+    class Program
+    {
+        const string QueueConnectionString = "Endpoint=sb://geekburguermarcio.servicebus.windows.net/;SharedAccessKeyName=ProductPolitcs;SharedAccessKey=cyOYn4AZECbFu1McvBM2/eMCFZvhEaQbZ/KgcAqEZrk=;";
+        const string QueuePath = "productbus";
+        static IQueueClient _queueClient;
+        static List<Task> PendingCompleteTasks = new List<Task>();
+        static void Main(string[] args)
+        {
+            //SendMessagesAsync().GetAwaiter().GetResult();
+            //Console.WriteLine("messages were sent");
+            //Console.ReadLine();
+
+            //ReceiveMessagesAsync().GetAwaiter().GetResult();
+            //Console.WriteLine("mensagens recebidas");
+            //Console.ReadLine();
+
+            if (args.Length <= 0 || args[0] == "sender")
+            {
+                SendMessagesAsync().GetAwaiter().GetResult();
+                Console.WriteLine("messages were sent");
+            }
+            else if (args[0] == "receiver")
+            {
+                ReceiveMessagesAsync().GetAwaiter().GetResult();
+                Console.WriteLine("messages were received");
+            }
+            else
+                Console.WriteLine("nothing to do");
+
+            Console.ReadLine();
+        }
+        private static async Task SendMessagesAsync()
+        {
+            //_queueClient = new QueueClient(QueueConnectionString, QueuePath);
+            //var messages = "Hi,Hello,Hey,How are you,Be Welcome"
+            //    .Split(",")
+            //    .Select(msg => {
+            //        Console.WriteLine($"Will send message: {msg}");
+            //        return new Message(Encoding.UTF8.GetBytes(msg));
+            //    })
+            //            .ToList();
+            //await _queueClient.SendAsync(messages);
+            //await _queueClient.CloseAsync();
+
+            var queueClient = new QueueClient(QueueConnectionString, QueuePath);
+            queueClient.OperationTimeout = TimeSpan.FromSeconds(10);
+            var messages = " Hi,Hello,Hey,How are you,Be Welcome"
+                .Split(',')
+                .Select(msg =>
+                {
+                    Console.WriteLine($"Will send message: {msg}");
+                    return new Message(Encoding.UTF8.GetBytes(msg));
+                })
+                .ToList();
+            var sendTask = queueClient.SendAsync(messages);
+            await sendTask;
+            CheckCommunicationExceptions(sendTask);
+            var closeTask = _queueClient.CloseAsync();
+            await closeTask;
+            CheckCommunicationExceptions(closeTask);
+        }
+
+        private static async Task ReceiveMessagesAsync()
+        {
+            //_queueClient = new QueueClient(QueueConnectionString, QueuePath);
+            //_queueClient.RegisterMessageHandler(MessageHandler,
+            //    new MessageHandlerOptions(ExceptionHandler) { AutoComplete = false });
+            //Console.ReadLine();
+
+            //await _queueClient.CloseAsync();
+
+            _queueClient = new QueueClient(QueueConnectionString,QueuePath, ReceiveMode.PeekLock);
+            _queueClient.RegisterMessageHandler(MessageHandler, new MessageHandlerOptions(ExceptionHandler) { AutoComplete = false });
+            Console.ReadLine();
+            Console.WriteLine($" Request to close async. Pending tasks:{ PendingCompleteTasks.Count} ");
+            await Task.WhenAll(PendingCompleteTasks);
+            Console.WriteLine($"All pending tasks were completed");
+            var closeTask = _queueClient.CloseAsync();
+            await closeTask;
+            CheckCommunicationExceptions(closeTask);
+        }
+        private static Task ExceptionHandler(ExceptionReceivedEventArgs exceptionArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionArgs.Exception}.");
+            var context = exceptionArgs.ExceptionReceivedContext;
+            Console.WriteLine($"Endpoint:{context.Endpoint}, Path:{context.EntityPath}, Action:{context.Action}");
+            return Task.CompletedTask;
+        }
+        private static async Task MessageHandler(Message message, CancellationToken cancellationToken)
+        {
+            int count = 0;
+            //Console.WriteLine($"Received message:{ Encoding.UTF8.GetString(message.Body)}");
+            //await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            Console.WriteLine($"Received message{ Encoding.UTF8.GetString(message.Body)}");
+
+            if (cancellationToken.IsCancellationRequested || _queueClient.IsClosedOrClosing)
+                return;
+
+            Console.WriteLine($"task {count++}");
+            Task PendingTask;
+            lock (PendingCompleteTasks)
+            {
+                PendingCompleteTasks.Add(_queueClient.CompleteAsync(message.SystemProperties.LockToken));
+                PendingTask = PendingCompleteTasks.LastOrDefault();
+            }
+            Console.WriteLine($"calling complete for task {count}");
+            await PendingTask;
+            Console.WriteLine($"remove task {count} from task queue");
+            PendingCompleteTasks.Remove(PendingTask);
+        }
+        public static bool CheckCommunicationExceptions(Task task)
+        {
+            if (task.Exception == null || task.Exception.InnerExceptions.Count == 0) return true;
+
+            task.Exception.InnerExceptions.ToList()
+                    .ForEach(innerException =>
+                    {
+                        Console.WriteLine($"Error in SendAsync task:{ innerException.Message}.Details:{ innerException.StackTrace}");
+                        if (innerException is ServiceBusCommunicationException)
+                            Console.WriteLine("Connection Problem with Host");
+                    });
+
+            return false;
+        }
+    }
+}
